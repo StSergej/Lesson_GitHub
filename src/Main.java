0224206e4096a6a9a71f067f23a77e2804839837
StSergej <st7cub@gmail.com>
2022-11-25 00:08:50 +0200

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+class Calculator {
+    int a = 12, b = 33, c = 57;
+
+    public int calculator() {
+        int result = (a + b + c) / 3;
+        return result;
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, Git!");
+
+        Calculator middle = new Calculator();
+        System.out.println("Среднее значение равно: " + middle.calculator());
     }
 }
